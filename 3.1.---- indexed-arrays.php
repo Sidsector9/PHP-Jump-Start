dbe7044ee132ee58bdf0385c6db6187aed32cb59
Sidsector9 <siddharth.thevaril@gmail.com>
2017-03-10 17:57:24 +0530

    Added content for indexed arrays

---
+++ 3.1.---- indexed-arrays.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * In 3.0, you learnt how to declare (create) an empty array.
+ * Now let us look into how an indexed array looks like.
+ */
+
+
+
+$an_indexed_array = array( 'First', 'Second', 'Third' );
+
+// --------------------------------
+// | 'First' | 'Second' | 'Third' |		<--- Values
+// --------------------------------
+//		0		   1	 	 2			<--- Location index
